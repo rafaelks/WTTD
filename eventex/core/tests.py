b800978b5154857a75c1456efbdd2829234b6836
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-09-15 20:58:36 -0300

    Move all core app to inside eventex

---
+++ eventex/core/tests.py
@@ -0,0 +1,21 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+
+
+class HomepageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        'GET / must return status code 200'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Homepage must use template index.html'
+        self.assertTemplateUsed(self.resp, 'index.html')
--- eventex/urls.py
+++ eventex/urls.py
@@ -4,12 +4,6 @@ from django.conf.urls import patterns, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('core.views',
-    url(r'^$', 'homepage', name='homepage'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('',
+    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
 )
